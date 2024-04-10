ef63b5064bfa116376f80980abb997c299d74964
Rajput <nitin98811234@gmail.com>
2024-04-11 03:26:25 +0530

    Form validation completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
+import { valideData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [validateObj, setValidateObj] = useState({
+    emailValidMssg: null,
+    passwordValidMssg: null,
+    nameValidMssg: null,
+  });
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleBtnClick = () => {
+    const validateObj = isSignInForm
+      ? valideData(email.current.value, password.current.value)
+      : valideData(
+          email.current.value,
+          password.current.value,
+          true,
+          name?.current?.value
+        );
+
+    setValidateObj(validateObj);
+  };
+
   return (
-    <div>
+    <div className="bg-black bg-opacity-90">
       <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/6cefb2f5-90be-4f57-adc4-f6c3c579273d/3943990c-f4e0-4147-82ad-f531e2b547f3/IN-en-20240401-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
@@ -13,8 +36,12 @@ const Login = () => {
       </div>
       <Header />
 
-      <div className="absolute w-4/12 h-auto bg-black top-1/4 right-0 left-0 mx-auto my-auto text-white p-10 bg-opacity-80 rounded-md">
-        <form>
+      <div className="absolute w-1/3 h-auto bg-black top-28 right-0 left-0 mx-auto my-auto text-white p-20 bg-opacity-80 rounded-md">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
           <h1 className="font-bold text-3xl">
             {isSignInForm ? "Sign In" : "Sign Up"}
           </h1>
@@ -23,22 +50,39 @@ const Login = () => {
               className="bg-black w-full mt-8 px-4 py-2 border-current	border rounded-md"
               type="text"
               placeholder="Name"
+              ref={name}
             />
           )}
+          {!isSignInForm && validateObj.nameValidMssg && (
+            <p className="text-red-700">{"💥" + validateObj.nameValidMssg}</p>
+          )}
           <input
-            className="bg-black w-full mt-8 my-4 px-4 py-2 border-current	 border rounded-md"
+            className="bg-black w-full mt-8  px-4 py-2 border-current	 border rounded-md"
             type="text"
             placeholder="Email or phone number"
+            ref={email}
           />
+          {validateObj.emailValidMssg && (
+            <p className="text-red-700 ">{"💥" + validateObj.emailValidMssg}</p>
+          )}
           <input
-            className="bg-black w-full my-4 px-4 py-2 border-current	border rounded-md"
+            className="bg-black w-full mt-8 px-4 py-2 border-current	border rounded-md"
             type="text"
             placeholder="Password"
+            ref={password}
           />
-          <button className="bg-red-700 w-full my-4 p-2 rounded-lg text-lg font-medium">
+          {validateObj.passwordValidMssg && (
+            <p className="text-red-700 ">
+              {"💥" + validateObj.passwordValidMssg}
+            </p>
+          )}
+          <button
+            className="bg-red-700 w-full my-4 p-2 rounded-lg text-lg font-medium"
+            onClick={handleBtnClick}
+          >
             {isSignInForm ? "Sign In" : "Sign Up"}
           </button>
-          <p className="text-gray-400 font-light">
+          <p className="text-gray-400 font-light my-4">
             {isSignInForm ? "New to Netflix? " : "Already registered? "}
             <span
               className="cursor-pointer font-medium text-white"
---
+++ src/utils/validate.js
@@ -0,0 +1,33 @@
+export const valideData = (
+  email,
+  password,
+  validateName = false,
+  name = null
+) => {
+  const isNameValid = /^[a-zA-Z]+ [a-zA-Z]+$/.test(name);
+  const isEmailValid = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email);
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      password
+    );
+
+  return validateName
+    ? {
+        emailValidMssg: isEmailValid
+          ? null
+          : "Please enter a valid email address.",
+        passwordValidMssg: isPasswordValid
+          ? null
+          : "Your password must contain minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.",
+        nameValidMssg: isNameValid ? null : "Please enter valid name.",
+      }
+    : {
+        emailValidMssg: isEmailValid
+          ? null
+          : "Please enter a valid email address.",
+        passwordValidMssg: isPasswordValid
+          ? null
+          : "Your password must contain minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.",
+        nameValidMssg: null,
+      };
+};
