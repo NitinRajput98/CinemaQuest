3b0b94c60786271b566bb3f40fb12a269febe21b
Rajput <nitin98811234@gmail.com>
2024-04-14 19:26:18 +0530

    Random movie selection added

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,8 +5,13 @@ import { useSelector } from "react-redux";
 const MainContainer = () => {
   const movies = useSelector((store) => store?.movies?.nowPlayingMovies);
 
+  const getRandomMovie = (movies) => {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    return movies[randomIndex];
+  };
+
   if (!movies) return;
-  const mainMovie = movies?.[0];
+  const mainMovie = getRandomMovie(movies);
   return (
     <div>
       <VideoTitle title={mainMovie.title} overview={mainMovie.overview} />
